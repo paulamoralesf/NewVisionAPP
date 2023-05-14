c58bf18c4bfa5658f383388e2eab39245ad747f9
Paula <paula@preonmics.com>
2023-05-14 18:04:46 +0200

    Add styles to the app and necessary components. Tailwind installed.

--- UI-NewVision/UI-NewVision/src/app/app-routing.module.ts
+++ UI-NewVision/UI-NewVision/src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'provider',
+    loadChildren: () =>
+      import('./providers/providers.module').then((m) => m.ProvidersModule),
+  },
+  {
+    path: 'client',
+    loadChildren: () =>
+      import('./clients/clients.module').then((m) => m.ClientsModule),
+  },
+  {
+    path: 'orders',
+    loadChildren: () =>
+      import('./orders/orders.module').then((m) => m.OrdersModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
